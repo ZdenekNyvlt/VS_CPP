91e315a2d4eae20ac0003880d142d9f0d2088be3
zdena <zdenek.nyvlt@gmail.com>
2020-12-21 17:55:36 +0100

    [ADD] Intial files

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+#define LOOP_COUNTER_MAX 5
+
+int main()
+{
+    for (unsigned int loop_counter = 0; loop_counter < LOOP_COUNTER_MAX; loop_counter++)
+    cout << "Hello World" << endl;
+}
